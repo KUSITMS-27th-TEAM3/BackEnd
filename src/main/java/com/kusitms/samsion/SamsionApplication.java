c9d73af6a024252092eb8070121ae8505d1b8383
tlarbals824 <simkm0824@naver.com>
2023-04-04 12:48:55 +0900

    [feat] project setting

---
+++ src/main/java/com/kusitms/samsion/SamsionApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.samsion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsionApplication.class, args);
+	}
+
+}
