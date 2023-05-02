3053d8ba3600a1b441b3d649d7d5fc1981965aa4
seeunjang <wisjang1@naver.com>
2023-05-02 13:02:21 +0900

    [feat] 추석처리 및 index.adoc 작성

--- src/test/java/com/kusitms/samsion/SamsionApplicationTests.java
+++ src/test/java/com/kusitms/samsion/SamsionApplicationTests.java
@@ -3,7 +3,7 @@ package com.kusitms.samsion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SamsionApplicationTests {
 
 	@Test
