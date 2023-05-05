2c393594973fd944dc43a0938cd02b1e7b18f897
tlarbals824 <simkm0824@naver.com>
2023-05-05 12:39:24 +0900

    [feat] email로 사용자 조회 로직 추가

--- src/main/java/com/kusitms/samsion/domain/user/repository/UserRepository.java
+++ src/main/java/com/kusitms/samsion/domain/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ import com.kusitms.samsion.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
+
+	boolean existsByEmail(String email);
 }
