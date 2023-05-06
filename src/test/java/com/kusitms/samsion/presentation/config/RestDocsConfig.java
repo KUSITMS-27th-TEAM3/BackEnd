4f129b63b1ca95f4f357aefd5a0dce2762b897e6
tlarbals824 <simkm0824@naver.com>
2023-05-07 00:13:34 +0900

    [refactor] rest docs 파일 위치 변경

--- src/test/java/com/kusitms/samsion/presentation/config/RestDocsConfig.java
+++ src/test/java/com/kusitms/samsion/presentation/config/RestDocsConfig.java
@@ -1,4 +1,4 @@
-package com.kusitms.samsion.config;
+package com.kusitms.samsion.presentation.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
