cad1a68211413cf174b35d377bd8d9035032e274
tlarbals824 <simkm0824@naver.com>
2023-05-07 00:00:28 +0900

    [fix] 사용자 db 저장 , 매퍼 클래스 이름 변경

---
+++ src/main/java/com/kusitms/samsion/application/user/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.kusitms.samsion.application.user.mapper;
+
+import com.kusitms.samsion.application.user.dto.request.UserSignUpRequest;
+import com.kusitms.samsion.common.annotation.Mapper;
+import com.kusitms.samsion.domain.user.entity.User;
+
+@Mapper
+public class UserMapper {
+
+	public User toEntity(UserSignUpRequest request){
+		return User.builder()
+			.email(request.getEmail())
+			.nickname(request.getNickname())
+			.build();
+	}
+
+}
