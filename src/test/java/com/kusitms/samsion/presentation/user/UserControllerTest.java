61aadecc1dfb9a568f75cf70b8251c582d75f0cf
tlarbals824 <simkm0824@naver.com>
2023-05-09 16:41:02 +0900

    [test] 테스트 파일 구조 수정

--- src/test/java/com/kusitms/samsion/presentation/user/UserControllerTest.java
+++ src/test/java/com/kusitms/samsion/presentation/user/UserControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,6 +23,7 @@ import com.kusitms.samsion.common.consts.ApplicationConst;
 import com.kusitms.samsion.presentation.config.CommonRestDocs;
 
 @WebMvcTest(UserController.class)
+@DisplayName("UserController 테스트")
 public class UserControllerTest extends CommonRestDocs {
 
 	@MockBean
