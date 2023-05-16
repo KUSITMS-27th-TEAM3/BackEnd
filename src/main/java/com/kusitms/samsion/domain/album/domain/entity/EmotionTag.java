7867625eb738b227cfab2a54b4f0a0afd08e1052
tlarbals824 <simkm0824@naver.com>
2023-05-16 21:52:06 +0900

    [feat] Tag 테이블 생성후 Album과 연결 로직 생성

---
+++ src/main/java/com/kusitms/samsion/domain/album/domain/entity/EmotionTag.java
@@ -0,0 +1,21 @@
+package com.kusitms.samsion.domain.album.domain.entity;
+
+public enum EmotionTag {
+
+	COZY("아늑함"),
+	HAPPY("행복함"),
+	JOY("즐거움"),
+	MISS("그리움"),
+	TOUCHING("감동적"),
+	COMFORTABLE("편안함"),
+	CHEERFUL("유쾌함"),
+	PROUD("자랑스러움"),
+	LONELY("외로움"),
+	LOVELY("사랑스러움");
+
+	private String description;
+
+	EmotionTag(String description) {
+		this.description = description;
+	}
+}
