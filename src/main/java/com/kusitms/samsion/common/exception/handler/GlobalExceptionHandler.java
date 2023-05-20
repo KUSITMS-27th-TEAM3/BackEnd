410bffdb5c8d08b7e3b5e6600fc9a07563ca5737
tlarbals824 <simkm0824@naver.com>
2023-05-20 19:57:35 +0900

    [feat] 예외발생시 로그 남기도록함

--- src/main/java/com/kusitms/samsion/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kusitms/samsion/common/exception/handler/GlobalExceptionHandler.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.kusitms.samsion.common.exception.BusinessException;
 import com.kusitms.samsion.common.exception.dto.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<ErrorResponse> handleJwtException(BusinessException e) {
 		final ErrorResponse errorResponse = ErrorResponse.from(e);
+		log.info("error log = {} {}",e.getError().getErrorCode(), e.getError().getMessage() );
 		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 }
