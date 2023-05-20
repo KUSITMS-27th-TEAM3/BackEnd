2e53749e50a1337d363d8eb908966df1605f4ca7
tlarbals824 <simkm0824@naver.com>
2023-05-20 20:05:03 +0900

    [feat] 댓글 수 카운트 메서드 캐싱, 댓글 추가 및 삭제시 캐시 데이터 삭제

--- src/main/java/com/kusitms/samsion/domain/comment/domain/service/CommentDeleteService.java
+++ src/main/java/com/kusitms/samsion/domain/comment/domain/service/CommentDeleteService.java
@@ -1,6 +1,9 @@
 package com.kusitms.samsion.domain.comment.domain.service;
 
+import org.springframework.cache.annotation.CacheEvict;
+
 import com.kusitms.samsion.common.annotation.DomainService;
+import com.kusitms.samsion.common.consts.CachingStoreConst;
 import com.kusitms.samsion.domain.comment.domain.entity.Comment;
 import com.kusitms.samsion.domain.comment.domain.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,7 @@ public class CommentDeleteService {
 
     private final CommentRepository commentRepository;
 
+    @CacheEvict(value = CachingStoreConst.COMMENT_COUNT_CACHE_NAME, key = "#comment.albumId")
     public void deleteComment(Comment comment) {
 
         commentRepository.deleteById(comment.getId());
--- src/main/java/com/kusitms/samsion/domain/comment/domain/service/CommentQueryService.java
+++ src/main/java/com/kusitms/samsion/domain/comment/domain/service/CommentQueryService.java
@@ -1,10 +1,12 @@
 package com.kusitms.samsion.domain.comment.domain.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kusitms.samsion.common.annotation.DomainService;
+import com.kusitms.samsion.common.consts.CachingStoreConst;
 import com.kusitms.samsion.common.exception.Error;
 import com.kusitms.samsion.domain.comment.domain.entity.Comment;
 import com.kusitms.samsion.domain.comment.domain.exception.CommentNotFoundException;
@@ -27,7 +29,7 @@ public class CommentQueryService {
         return commentRepository.findByAlbumId(pageable, albumId);
     }
 
-
+    @Cacheable(value = CachingStoreConst.COMMENT_COUNT_CACHE_NAME, key = "#albumId")
     public long getCommentCountByAlbumId(Long albumId){
         return commentRepository.countByAlbumId(albumId);
     }
