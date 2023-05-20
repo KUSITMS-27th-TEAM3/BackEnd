bf371e553e04df76ea51a66364a73f618cadbc07
tlarbals824 <simkm0824@naver.com>
2023-05-20 20:05:27 +0900

    [feat] 공감 수 카운트 메서드 캐싱, 공감 추가 및 삭제시 캐시 데이터 삭제

--- src/main/java/com/kusitms/samsion/domain/empathy/domain/service/EmpathyDeleteService.java
+++ src/main/java/com/kusitms/samsion/domain/empathy/domain/service/EmpathyDeleteService.java
@@ -1,8 +1,10 @@
 package com.kusitms.samsion.domain.empathy.domain.service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kusitms.samsion.common.annotation.DomainService;
+import com.kusitms.samsion.common.consts.CachingStoreConst;
 import com.kusitms.samsion.domain.empathy.domain.repository.EmpathyRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ public class EmpathyDeleteService {
 
 	private final EmpathyRepository empathyRepository;
 
+	@CacheEvict(value = CachingStoreConst.EMPATHY_COUNT_CACHE_NAME, key = "#comment.albumId")
 	public void deleteEmpathy(Long userId, Long albumId){
 		empathyRepository.deleteByUserIdAndAlbumId(userId, albumId);
 	}
--- src/main/java/com/kusitms/samsion/domain/empathy/domain/service/EmpathyQueryService.java
+++ src/main/java/com/kusitms/samsion/domain/empathy/domain/service/EmpathyQueryService.java
@@ -1,8 +1,10 @@
 package com.kusitms.samsion.domain.empathy.domain.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kusitms.samsion.common.annotation.DomainService;
+import com.kusitms.samsion.common.consts.CachingStoreConst;
 import com.kusitms.samsion.domain.empathy.domain.repository.EmpathyRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,8 @@ public class EmpathyQueryService {
 		return empathyRepository.existsByUserIdAndAlbumId(userId, albumId);
 	}
 
+
+	@Cacheable(value = CachingStoreConst.EMPATHY_COUNT_CACHE_NAME, key = "#albumId")
 	public long getEmpathyCountByAlbumId(Long albumId){
 		return empathyRepository.countByAlbumId(albumId);
 	}
