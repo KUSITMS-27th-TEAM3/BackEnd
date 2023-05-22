a45aaf0334add759851d09d59801dde110e4993c
tlarbals824 <simkm0824@naver.com>
2023-05-22 21:41:37 +0900

    [refactor] 불필요한 메서드 제거

--- src/main/java/com/kusitms/samsion/domain/album/domain/service/AlbumImageUpdateService.java
+++ src/main/java/com/kusitms/samsion/domain/album/domain/service/AlbumImageUpdateService.java
@@ -1,15 +1,14 @@
 package com.kusitms.samsion.domain.album.domain.service;
 
-import java.util.List;
-
-import org.springframework.transaction.annotation.Transactional;
-
 import com.kusitms.samsion.common.annotation.DomainService;
 import com.kusitms.samsion.domain.album.domain.entity.Album;
 import com.kusitms.samsion.domain.album.domain.entity.AlbumImage;
 import com.kusitms.samsion.domain.album.domain.repository.AlbumImageRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @DomainService
 @RequiredArgsConstructor
@@ -18,8 +17,9 @@ public class AlbumImageUpdateService {
 
 	private final AlbumImageRepository albumImageRepository;
 
-	public void updateAlbumImage(Album album, List<AlbumImage> albumImageList){
-		album.changeAllImage(albumImageList);
+	public void updateAlbumImage(Album album,List<String> oldImageList, List<String> newImageList){
+		List<AlbumImage> albumImageList = oldImageList.stream().map(imageUrl->new AlbumImage(imageUrl,album)).collect(Collectors.toList());
+		albumImageList.addAll(newImageList.stream().map(imageUrl->new AlbumImage(imageUrl,album)).collect(Collectors.toList()));
 		albumImageRepository.saveAll(albumImageList);
 	}
 }
