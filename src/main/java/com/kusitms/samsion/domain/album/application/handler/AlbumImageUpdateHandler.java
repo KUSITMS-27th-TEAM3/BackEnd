5cc93ebc3ca56e7d146c5e6dde9cbbe648cf96e6
tlarbals824 <simkm0824@naver.com>
2023-05-23 20:08:47 +0900

    [fix] 애노테이션 정리

--- src/main/java/com/kusitms/samsion/domain/album/application/handler/AlbumImageUpdateHandler.java
+++ src/main/java/com/kusitms/samsion/domain/album/application/handler/AlbumImageUpdateHandler.java
@@ -7,7 +7,6 @@ import com.kusitms.samsion.domain.album.domain.entity.Album;
 import com.kusitms.samsion.domain.album.domain.service.AlbumImageUpdateService;
 import com.kusitms.samsion.domain.album.domain.service.AlbumQueryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -23,7 +22,6 @@ public class AlbumImageUpdateHandler {
 	private final AlbumImageUpdateService albumImageUpdateService;
 	private final S3UploadService s3UploadService;
 
-	@Async
 	@TransactionalEventListener
 	public void updateAlbumImage(AlbumImageUpdateRequest albumImageUpdateRequest) {
 		final Album album = albumQueryService.findAlbumById(albumImageUpdateRequest.getAlbumId());
